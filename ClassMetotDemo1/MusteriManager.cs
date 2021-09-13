1b7284823f0c228c87811db6e27dbde0d4b8ad0a
Hatice Elçin Bozdağ <elcinhatice@hotmail.com>
2021-09-13 20:26:29 +0300

    Metot ödevi

---
+++ ClassMetotDemo1/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo1
+{
+    class MusteriManager
+    {
+       public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Kişi eklendi : " + musteri.MusteriAdi);
+
+        }
+
+        public void Listele (Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi);
+                Console.WriteLine(musteri.MusteriId);
+                Console.WriteLine(musteri.MusteriYas);
+            }
+        }
+       
+
+       
+
+    }
+}
---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Elçin";
+            musteri1.MusteriId = 566;
+            musteri1.MusteriYas = 20;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Türkü";
+            musteri2.MusteriId = 123;
+            musteri2.MusteriYas = 20;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi);
+                Console.WriteLine(musteri.MusteriId);
+                Console.WriteLine(musteri.MusteriYas);
+            }
+
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            musteriManager.Listele(musteriler);
+
+
+        }
+    }
+}
